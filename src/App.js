1f8900c7199fd7f45e7314bec9cb85a87b85e447
EmirlanDogdurbaev <jigit38@gmail.com>
2021-02-24 14:39:29 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import './App.css';
-function Person(props) {
-  let smokes = <strong>Ne kurit</strong>
-  if (props.smokes) {
-    smokes = <strong>Kurit</strong>
-  }
- 
- return <div className="person">
-   <h1>{ props.name }</h1>
-   <div className="age">{ props.age } years old</div>
-   <p className="hobby">{ props.children }</p>
-   <strong>{ smokes }</strong>
- </div>;
-}
-
+import React from 'react';
+import HelloWorld from "./components/HelloWorld";
+import ByeWorld from "./components/ByeWorld";
+import Chunga from "./components/Chunga";
 
 function App() {
   return (
     <div className="App">
-    <Person name="Elon Musk" age={41}>Listens to <strong>music</strong></Person>
-    <Person name="Jeff Besos" age={55} smokes={true} />
-    <Person name="Bill Gates" age={56} hobbies={["Singing", "Dancing"]}>Travels</Person>
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <ByeWorld/>      <ByeWorld />      <ByeWorld />      <ByeWorld/>
+      <Chunga />      <Chunga />      <Chunga />      <Chunga />
     </div>
   );
 }
