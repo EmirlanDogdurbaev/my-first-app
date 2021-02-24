a1fbf68a609137b79c32f9a53a03fd3308f02790
EmirlanDogdurbaev <jigit38@gmail.com>
2021-02-24 15:01:55 +0300

    class-work cpmponents

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import HelloWorld from "./components/HelloWorld";
 import ByeWorld from "./components/ByeWorld";
 import Chunga from "./components/Chunga";
+import Hochu from './components/Hochu';
+import Books from './components/Books';
+import Song from './components/Song';
+
+
 
 function App() {
   return (
     <div className="App">
       <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
-      <ByeWorld/>      <ByeWorld />      <ByeWorld />      <ByeWorld/>
-      <Chunga />      <Chunga />      <Chunga />      <Chunga />
+      <ByeWorld/>     
+      <Chunga />    
+      <Hochu v = "more" />  
+      <Hochu v = "kosmos" />  
+      <Books name = "Harry Potter" author = "J.R." />
+      <Song name = "Somoeone you loved" artist = "Lewis Capaldi" />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,9 @@
+function Song(props) {
+    return (
+    <div>
+        <h1>{props.name}</h1>
+        <strong>{props.artist}</strong>
+    </div>
+    )}
+
+export default Song;
